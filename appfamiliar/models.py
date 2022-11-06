47f5dd9e9fc65bc0945930099110b1e91b8ff87b
Israel Balbuena <israel.balbuena@alumnos.cide.edu>
2022-11-06 13:21:56 -0600

    VERSION INICIAL DEL PROYECTO MTV

---
+++ appfamiliar/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Familiares(models.Model):
+
+    parentesco = models.CharField(max_length = 50)
+
+    nombre = models.CharField(max_length = 50)
+
+    apellido = models.CharField(max_length = 50)
+
+    edad  = models.IntegerField()   
+
+    fecha_nacimiento = models.DateTimeField()
+
+    altura = models.IntegerField()
+
+    email = models.EmailField(max_length = 50)
+
+
+
