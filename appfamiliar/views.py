27a703e857a931ac77ba74321247edaa5d980ed3
Israel Balbuena <israel.balbuena@alumnos.cide.edu>
2022-11-06 19:00:44 -0600

    agregue cuatrofamiliares, falta pasarlos al template

--- appfamiliar/views.py
+++ appfamiliar/views.py
@@ -5,11 +5,11 @@ from appfamiliar.models import Familiares
 
 def greet(request):
 
-    mama = Familiares(1,"madre","Francisca","Balbuena", 61, "1961-12-03",1.65,"francisbal@live.com.mx")
+    # mama = Familiares(1,"madre","Francisca","Balbuena", 61, "1961-12-03",1.65,"francisbal@live.com.mx")
     
-    mama.save()
+    # mama.save()
  
-    datos = {"nombre": mama.nombre, "parentesco": mama.parentesco,"fecha_nacimiento":mama.fecha_nacimiento}
+    datos = {"nombre":Familiares.objects.get(nombre=1)}
 
     plantilla = loader.get_template("plantilla.html")
 
